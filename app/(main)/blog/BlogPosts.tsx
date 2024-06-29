47d5dc768defbf87926cb7188fcd291744d384ad
oiov <stacystayfine718@gmail.com>
2024-06-29 14:26:37 +0800

    fixup

--- app/(main)/blog/BlogPosts.tsx
+++ app/(main)/blog/BlogPosts.tsx
@@ -6,11 +6,11 @@ import { getLatestBlogPosts } from '~/sanity/queries'
 import { BlogPostCard } from './BlogPostCard'
 
 export async function BlogPosts({ limit = 5 }) {
-  const posts = await getLatestBlogPosts({ limit, forDisplay: true }) || []
+  const posts = (await getLatestBlogPosts({ limit, forDisplay: true })) || []
   const postIdKeys = posts.map(({ _id }) => kvKeys.postViews(_id))
 
   let views: number[] = []
-  if (env.VERCEL_ENV === 'development') {
+  if (env.VERCEL_ENV === 'development' || posts.length === 0) {
     views = posts.map(() => Math.floor(Math.random() * 1000))
   } else {
     if (postIdKeys.length > 0) {
