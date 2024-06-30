faae601c478a26b4171e026fff29ebb602a921e9
oiov <stacystayfine718@gmail.com>
2024-06-30 10:18:17 +0800

    upd

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ const config = {
         '@typescript-eslint/no-unsafe-call': 'off',
         '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-unsafe-assignment': 'off',
+        'simple-import-sort/imports': 'off',
       },
     },
   ],
