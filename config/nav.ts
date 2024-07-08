00f393a161375603bd2d9e9adcbe3c792f5603d0
oiov <stacystayfine718@gmail.com>
2024-07-08 11:24:24 +0800

    upd

--- config/nav.ts
+++ config/nav.ts
@@ -4,5 +4,6 @@ export const navigationItems = [
   { href: '/projects', text: '项目' },
   { href: '/guestbook', text: '留言墙' },
   { href: 'https://wr.do/ai', text: '公益AI' },
+  { href: 'https://www.oiov.dev/blog/llk', text: '流量卡' },
   // { href: '/about', text: '关于' },
 ]
