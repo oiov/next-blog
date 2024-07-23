848c4676bf8316228d4b2492199989f584fa7969
oiov <stacystayfine718@gmail.com>
2024-07-23 15:42:04 +0800

    upd

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -24,10 +24,10 @@ export default function ProjectsPage() {
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          我的项目冒险之旅
+          我的项目
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-          你做出一个很酷的东西出来，想给他起什么名都可以。
+          建站，建站，建站！你做出一个很酷的东西出来，想给他起什么名都可以。
         </p>
       </header>
       <div className="mt-16 sm:mt-20">
