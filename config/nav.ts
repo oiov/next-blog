9bbf0cc5012c9cfe904bc1ff0e24dfc20eeaad8e
oiov <oiov@duck.com>
2024-10-28 11:04:35 +0800

    upd

--- config/nav.ts
+++ config/nav.ts
@@ -3,7 +3,7 @@ export const navigationItems = [
   { href: '/blog', text: '笔记' },
   { href: '/projects', text: '项目' },
   { href: '/guestbook', text: '留言墙' },
-  // { href: 'https://wr.do/ai', text: '公益AI' },
+  { href: 'https://wr.do/s/ai', text: '公益AI' },
   // { href: 'https://www.oiov.dev/blog/llk', text: '大流量卡' },
   // { href: '/about', text: '关于' },
 ]
